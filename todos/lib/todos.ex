e57923564a114a21e18f3de8ce4e87231e1dc0ed
Josh <zylo.codes@gmail.com>
2023-03-25 16:25:55 -0500

    Create and search Todos

--- todos/lib/todos.ex
+++ todos/lib/todos.ex
@@ -2,13 +2,30 @@
 
 defmodule Todos do
   def create_todos do
-    { num, _ } =
-      IO.gets("\nHow many tasks do you want to add?")
+    { num_tasks, _ } =
+      IO.gets("How many tasks do you want to add? ")
         |> Integer.parse()
-    num
+    for _i <- 1..num_tasks do
+      IO.gets("Enter a task: ") |> String.trim()
+    end
   end
 
   def temp_todos do
     ["learn elixir", "learn rust", "learn c++"]
+  end
+
+  def contains?(tasks, task) do
+    Enum.member?(tasks, task)
+  end
+
+  def keyword_search(tasks, word) do
+    for task <- tasks, String.contains?(task, word) do
+      task
+    end
+  end
+
+  def random_task(tasks) do
+    [task] = Enum.take_random(tasks, 1)
+    task
   end
 end
