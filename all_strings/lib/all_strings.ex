99d1b75fefb274d55acb4c1312785f85ec8b5a91
Josh <zylo.codes@gmail.com>
2023-04-03 22:16:11 -0500

    contact and interpolate

--- all_strings/lib/all_strings.ex
+++ all_strings/lib/all_strings.ex
@@ -12,7 +12,19 @@ defmodule AllStrings do
       :world
 
   """
-  def hello do
-    :world
+  def string_stuff do
+    # concatination
+    "Hello "<> "world" <> "."
+
+    # interpolation
+    "6 * 7 = #{6 * 7}"
+
+    "Can interpolate lots of things, like booleans #{true} and lists #{["test", "test1", 2323]}"
+
+    """
+    multi
+    line
+    syntax
+    """
   end
 end
